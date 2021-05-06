3e0555bb4f1649cda772237cdb68d4742176d292
Carlos Guzmán <cags84@gmail.com>
2021-05-06 15:16:05 +0000

    Added babel config and feateures

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,22 @@ const path = require('path');
 module.exports = {
   mode: 'none',
   entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
   output: {
-    path: path.resolve(__dirname, 'dist/'),
+    path: path.resolve(__dirname, './dist/'),
     filename: 'bundle.js',
   },
   devServer: {
